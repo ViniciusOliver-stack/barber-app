8aac867f92813ea7ed61cf41563cf852816db08c
Vinicius Oliveira <vinicius.so.contato@hotmail.com>
2024-08-25 16:40:39 -0300

    feat: component footer

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent } from "./ui/card"
+
+export function Footer() {
+  const date = new Date()
+  const year = date.getFullYear()
+
+  return (
+    <footer>
+      <Card className="border-none">
+        <CardContent className="border-t border-gray01 px-5 py-6">
+          <p className="text-sm">{year} Copyright Vinicius Oliver 💜</p>
+        </CardContent>
+      </Card>
+    </footer>
+  )
+}
