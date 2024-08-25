16e6dd6b7f663f8af8e7a33975ba5bc002f36f38
Vinicius Oliveira <vinicius.so.contato@hotmail.com>
2024-08-25 16:44:01 -0300

    feat: add new components

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,25 @@ import { SearchIcon } from "lucide-react"
 {
   /*Components */
 }
+import { Footer } from "@/components/footer"
 import { Header } from "@/components/header"
 import { Input } from "@/components/ui/input"
-import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Carousel } from "@/components/carousel"
-import { Card, CardContent } from "@/components/ui/card"
-import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { BookingItem } from "@/components/booking-item"
+import { quickSearchOptions } from "./_constants/search"
+import { BarbershopItem } from "@/components/barbershop-item"
 
 import { db } from "@/lib/prisma"
 
 export default async function Home() {
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+    take: 5,
+  })
 
   return (
     <div>
@@ -24,8 +31,11 @@ export default async function Home() {
         <h2 className="text-xl font-bold">Olá, Vinicius!</h2>
         <p>Segunda-feira, 22 de agosto.</p>
 
-        <div className="mt-6 flex flex-row items-center gap-1">
-          <Input placeholder="Faça a sua busca" className="border-gray01" />
+        <div className="mt-6 flex flex-row items-center gap-1.5">
+          <Input
+            placeholder="Buscar"
+            className="border-gray01 placeholder:text-gray-500"
+          />
           <Button
             size="icon"
             className="rounded-[8px] bg-primaryPurple px-3 py-2"
@@ -33,6 +43,25 @@ export default async function Home() {
             <SearchIcon />
           </Button>
         </div>
+
+        <Carousel>
+          <div className="mt-6 flex items-center gap-3">
+            {quickSearchOptions.map((option, index) => (
+              <Button
+                key={index}
+                className="gap-2 rounded-[8px] border border-gray01 bg-secondaryBlack"
+              >
+                <Image
+                  src={option.imageUrl}
+                  width={16}
+                  height={16}
+                  alt={option.title}
+                />
+                {option.title}
+              </Button>
+            ))}
+          </div>
+        </Carousel>
 
         <div className="relative mt-6 h-[150px] w-full">
           <Image
@@ -44,46 +73,28 @@ export default async function Home() {
         </div>
 
         {/* agendamentos */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card className="rounded-[8px] border-none bg-secondaryBlack">
-          <CardContent className="flex justify-between">
-            <div className="flex flex-col gap-2 py-5">
-              <Badge className="w-fit bg-primaryPurple/30 text-primaryPurple hover:bg-primaryPurple/30">
-                Confirmado
-              </Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://avatars.githubusercontent.com/u/124599?v=4" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid border-gray01 pl-5">
-              <p className="text-sm">Fevereiro</p>
-              <p className="text-3xl">06</p>
-              <p className="text-sm">09:45</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
         </h2>
-
-        {/* Para esconder o scrollbar, usamos uma div com a classe [&::-webkit-scrollbar]:hidden. */}
-        {/* <div className="flex items-center gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+        <Carousel>
           {barbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
-        </div> */}
+        </Carousel>
 
-        <Carousel barbershops={barbershops} />
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <Carousel>
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </Carousel>
       </div>
+
+      <Footer />
     </div>
   )
 }
