747b885f645ee88c2c8cc8fa98c5cf03e82a93e0
Vinicius Oliveira <vinicius.so.contato@hotmail.com>
2024-08-25 16:43:22 -0300

    feat: constant quick options

---
+++ app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    imageUrl: "/hair.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/beard.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/finishing.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/eyebrows.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "/massage.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/hydration.svg",
+    title: "Hidratação",
+  },
+]
